c3490f0b7ede098e8ee4b321541a879ce9dd1a83
KrishnaMurar <114321792+KrishnaMurar@users.noreply.github.com>
2023-03-08 21:26:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+   import ExpenseItem from './Components/ExpenseItem'
+
+function App() {
+  return (
+    <div>
+<h2>Expense Items</h2>
+<ExpenseItem>Krishna</ExpenseItem>
+ 
+</div>
+  );
+  }
+ export default App;
